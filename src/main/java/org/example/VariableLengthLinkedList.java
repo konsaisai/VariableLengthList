4b56fc29a3e6338a22df8de21fdbcd6e7312d7b9
Aya Imanishi <konsaisai@gmail.com>
2023-04-23 20:56:52 +0900

    VariableLengthLinkedList add

--- src/main/java/org/example/VariableLengthLinkedList.java
+++ src/main/java/org/example/VariableLengthLinkedList.java
@@ -8,7 +8,6 @@ class Element {
 public class VariableLengthLinkedList {
     private Element head;
     private Element tail;
-    //TODO
 
     VariableLengthLinkedList() {
         this.head = null;
@@ -16,18 +15,112 @@ public class VariableLengthLinkedList {
     }
 
     public int count() {
-        boolean bNext = true;
-        int count = 0;
-        Element wkEle = this.head.next;
-        while (bNext) {
-            if (wkEle.next == null) {
-                count ++;
-            } else {
-                bNext = false;
-            }
+        if (this.head == null) {
+            return 0;
+        }
+        int count = 1;
+        Element element = this.head;
+        while (element.next != null) {
+            element = element.next;
+            count ++;
         }
         return count;
     }
 
+    public boolean isEmpty() { return this.head == null; }
 
+    public int get(int index) throws IndexOutOfBoundsException {
+        if (this.head == null) {
+            throw new IndexOutOfBoundsException("Error!!!IndexOutOfBounds");
+        }
+        int i = 0;
+        Element element = this.head;
+        while (i < index && element.next != null) {
+            element = element.next;
+            i ++;
+        }
+        if (i != index) {
+            throw new IndexOutOfBoundsException("Error!!!IndexOutOfBounds");
+        }
+        return element.element;
+    }
+
+    public String toString() {
+        // 文字列の結合は+だと毎回文字列を作成してしまうためStringBuilderを使用する(速度を速くする)
+        StringBuilder sb = new StringBuilder("[");
+        Element element = new Element();
+        element = this.head;
+        while (element != null) {
+            sb.append(element.element);
+            element = element.next;
+            if (element != null ) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public void add(int value) {
+        Element element = new Element();
+        element.element = value;
+
+        // headがnullの場合
+        if (this.head == null) {
+            this.head = element;
+        } else {
+            this.tail.next = element;
+        }
+        this.tail = element;
+    }
+
+    public void addAll(int[] newElements) {
+        for (int element: newElements) {
+            add(element);
+        }
+    }
+
+    public void update(int index, int newElement) throws IndexOutOfBoundsException {
+        if (this.head == null) {
+            throw new IndexOutOfBoundsException("Error!!!IndexOutOfBounds");
+        }
+        int i = 0;
+        Element element = this.head;
+        while (i < index && element.next != null) {
+            element = element.next;
+            i ++;
+        }
+        if (i != index) {
+            throw new IndexOutOfBoundsException("Error!!!IndexOutOfBounds");
+        }
+        element.element = newElement;
+    }
+
+    public void remove(int index) throws IndexOutOfBoundsException {
+        if (this.head == null) {
+            throw new IndexOutOfBoundsException("Error!!!IndexOutOfBounds");
+        }
+
+        if (index == 0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        int i = 0;
+        Element element = this.head;
+        while (i < index - 1 && element.next != null) {
+            element = element.next;
+            i ++;
+        }
+        if (i != index - 1) {
+            throw new IndexOutOfBoundsException("Error!!!IndexOutOfBounds");
+        }
+
+        if (element.next.next == null) {
+            this.tail = element;
+            element.next = null;
+        } else {
+            element.next = element.next.next;
+        }
+    }
 }
--- src/test/java/org/example/VariableLengthLinkedListTest.java
+++ src/test/java/org/example/VariableLengthLinkedListTest.java
@@ -1,2 +1,95 @@
-package org.example;public class VariableLengthLinkedListTest {
+package org.example;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VariableLengthLinkedListTest {
+    @Test
+    @DisplayName("CountMethodTest ⇨ 0")
+    public void testCount_0() {
+        var list = new VariableLengthLinkedList();
+        assertEquals(0, list.count());
+    }
+
+    @Test
+    @DisplayName("CountMethodTest ⇨ 3")
+    public void testCount_3() {
+        var list = new VariableLengthLinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        assertEquals(3, list.count());
+    }
+
+    @Test
+    @DisplayName("isEmpty")
+    public void isEmpty() {
+        var list = new VariableLengthLinkedList();
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    @DisplayName("isNotEmpty")
+    public void isNotEmpty() {
+        var list = new VariableLengthLinkedList();
+        list.add(5);
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    @DisplayName("get")
+    public void getElement() {
+        var list = new VariableLengthLinkedList();
+        assertThrows(IndexOutOfBoundsException.class, () ->list.get(0));
+        list.add((1));
+        list.add((2));
+        list.add((3));
+        assertEquals(list.get(0), 1);
+        assertEquals(list.get(1), 2);
+        assertEquals(list.get(2), 3);
+        assertThrows(IndexOutOfBoundsException.class, () ->list.get(-1));
+        assertThrows(IndexOutOfBoundsException.class, () ->list.get(4));
+
+    }
+
+    @Test
+    @DisplayName("ToString")
+    public void ToString() {
+        var list = new VariableLengthLinkedList();
+        assertEquals("[]", list.toString());
+        list.add((1));
+        list.add((2));
+        list.add((3));
+        assertEquals("[1, 2, 3]", list.toString());
+    }
+
+    @Test
+    @DisplayName("addAll")
+    public void addAll() {
+        var list = new VariableLengthLinkedList();
+        list.addAll(new int[] {1, 2, 3, 4, 10});
+        assertEquals("[1, 2, 3, 4, 10]", list.toString());
+    }
+
+    @Test
+    @DisplayName("update")
+    public void update() {
+        var list = new VariableLengthLinkedList();
+        list.addAll(new int[] {1, 2, 3, 4, 10});
+        list.update(1, 5);
+        assertEquals("[1, 5, 3, 4, 10]", list.toString());
+    }
+
+    @Test
+    @DisplayName("remove")
+    public void remove() {
+        var list = new VariableLengthLinkedList();
+        list.addAll(new int[] {1, 2, 3, 4, 10});
+        list.remove(1);
+        assertEquals("[1, 3, 4, 10]", list.toString());
+        list.remove(0);
+        assertEquals("[3, 4, 10]", list.toString());
+        list.remove(2);
+        assertEquals("[3, 4]", list.toString());
+    }
 }
